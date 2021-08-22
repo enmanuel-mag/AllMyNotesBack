0ae9054f0614596d9dd903f23eec5464e75e3ab0
jcoboss <josuealexandercs15@gmail.com>
2021-08-22 15:13:46 -0500

    Add cors

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import cors from 'cors';
 import api from './api';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
